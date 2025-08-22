682c67ea760c6322be3d273092d9f4be6a768ebc
Akayosan <124552163+Akayosan@users.noreply.github.com>
2025-08-22 11:13:36 +0700

    Add client/src/lib/web3-config.ts

---
+++ client/src/lib/web3-config.ts
@@ -0,0 +1,27 @@
+export const MONAD_TESTNET = {
+  chainId: '0x279F', // 10143 in hex
+  chainIdNumber: 10143,
+  chainName: 'Monad Testnet',
+  nativeCurrency: {
+    name: 'MON',
+    symbol: 'MON',
+    decimals: 18,
+  },
+  rpcUrls: ['https://testnet-rpc.monad.xyz'],
+  blockExplorerUrls: ['https://testnet-explorer.monad.xyz'],
+};
+
+export const CONTRACTS = {
+  NFT_CONTRACT: '0x1e48772aD690310310Ed14e17F857bA1231b993e',
+  STL_TOKEN: '0xCCa33DE1509eFd76E008eBe93d957f18C969baBf',
+  STAKING_CONTRACT: '0x103379cbaf24895c122bbd615c5a1e944621587b',
+};
+
+export const NFT_METADATA = {
+  IPFS_URI: 'ipfs://bafkreieuy6dxdqpzxquytur2hk2eeqyigejccid3jouyvcd2jbmz53nhwi',
+  NAME: 'Scuttle',
+  RARITY: 'Ultra Rare',
+  IMAGE_URL: 'https://raw.githubusercontent.com/Akayosan/Game/refs/heads/main/Ultra.jpg',
+};
+
+export const LOGO_URL = 'https://raw.githubusercontent.com/Akayosan/Game/refs/heads/main/md.jpg';
